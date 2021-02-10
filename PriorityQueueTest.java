891cf67d39bf56e3f95825381279dff375bb47cd
Nils Streedain <business@nilsstreedain.com>
2021-02-10 01:42:54 -0800

    Added Character and String Tests

--- PriorityQueueTest.java
+++ PriorityQueueTest.java
@@ -13,50 +13,65 @@ class PriorityQueueTest {
 	private SimplePriorityQueue<Integer> manyIntegers;
 	private SimplePriorityQueue<Double> manyDoubles;
 	private SimplePriorityQueue<Character> manyCharacters;
+	private SimplePriorityQueue<String> manyStrings;
 
 	private Random rand;
-	private String allCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	
+	private String allCharacters = "abcdefghijklmnopqrstuvwxyz";
+
 	@BeforeEach
 	void TestSetup() {
 		rand = new Random();
-		
+
 		singleInteger = new SimplePriorityQueue<>();
-		singleInteger.insert(1);
 
 		manyIntegers = new SimplePriorityQueue<>();
 		for (int i = 0; i < 1000; i++)
 			manyIntegers.insert(rand.nextInt(999) + 1);
-		manyIntegers.insert(0);
 
 		manyDoubles = new SimplePriorityQueue<>();
 		for (int i = 0; i < 1000; i++)
 			manyDoubles.insert(rand.nextDouble() + 1.0);
-		manyDoubles.insert(0.0);
-		
+
 		manyCharacters = new SimplePriorityQueue<>();
 		for (int i = 0; i < 1000; i++)
 			manyCharacters.insert(allCharacters.charAt(rand.nextInt(26)));
-		manyCharacters.insert('A');
+
+		manyStrings = new SimplePriorityQueue<>();
+		for (int i = 0; i < 1000; i++) {
+			String tempString = "";
+			for (int j = 0; j < 10; j++)
+				tempString += allCharacters.charAt(rand.nextInt(26));
+			manyStrings.insert(tempString);
+		}
 	}
 
 	@Test
 	void testOneElement() {
+		singleInteger.insert(1);
 		assertTrue(singleInteger.findMin() == 1);
 	}
 
 	@Test
 	void testManyIntegers() {
+		manyIntegers.insert(0);
 		assertTrue(manyIntegers.findMin() == 0);
 	}
 
 	@Test
 	void testManyDoubles() {
+		manyDoubles.insert(0.0);
 		assertTrue(manyDoubles.findMin() == 0.0);
 	}
-	
+
 	@Test
 	void testManyCharacters() {
-		assertTrue(manyCharacters.findMin() == 'A');
+		manyCharacters.insert('a');
+		assertTrue(manyCharacters.findMin() == 'a');
+	}
+
+	@Test
+	void testManyStrings() {
+		manyStrings.insert("aaaaaaaaaa");
+		assertTrue(manyStrings.findMin().equals("aaaaaaaaaa"));
 	}
 }
--- SimplePriorityQueue.java
+++ SimplePriorityQueue.java
@@ -5,28 +5,27 @@ import java.util.Comparator;
 import java.util.NoSuchElementException;
 
 public class SimplePriorityQueue<E> implements PriorityQueue<E> {
-	
+
 	private int size = 0;
 	private Object[] arr;
 	private Comparator<? super E> cmp;
-	
+
 	private final int INITIAL_ARRAY_SIZE = 10;
 
 	@SuppressWarnings("unchecked")
 	public SimplePriorityQueue() {
-		arr =  (E[]) new Object[INITIAL_ARRAY_SIZE];
+		arr = (E[]) new Object[INITIAL_ARRAY_SIZE];
 		cmp = null;
 	}
 
 	@SuppressWarnings("unchecked")
 	public SimplePriorityQueue(Comparator<? super E> cmp) {
-		arr =  (E[]) new Object[INITIAL_ARRAY_SIZE];
+		arr = (E[]) new Object[INITIAL_ARRAY_SIZE];
 		this.cmp = cmp;
 	}
 
 	/**
-	 * Retrieves, but does not remove, the minimum element in this priority
-	 * queue.
+	 * Retrieves, but does not remove, the minimum element in this priority queue.
 	 * 
 	 * @return the minimum element
 	 * @throws NoSuchElementException if the priority queue is empty
@@ -38,7 +37,7 @@ public class SimplePriorityQueue<E> implements PriorityQueue<E> {
 			throw new NoSuchElementException();
 		return (E) arr[size - 1];
 	}
-	
+
 	/**
 	 * Retrieves and removes the minimum element in this priority queue.
 	 * 
@@ -64,21 +63,21 @@ public class SimplePriorityQueue<E> implements PriorityQueue<E> {
 	public void insert(Object item) {
 		E goal = (E) item;
 		int index = insertSearch(goal);
-		
+
 		if (arr.length == size) {
 			Object[] newArr = new Object[arr.length * 2];
 			for (int i = 0; i < arr.length; i++)
 				newArr[i] = arr[i];
 			arr = newArr;
 		}
-		
+
 		for (int i = size; i > index; i--)
 			arr[i] = arr[i - 1];
-		
+
 		arr[index] = item;
 		size++;
 	}
-	
+
 	/**
 	 * Searches for the index to place the item in insert using a binary search.
 	 * 
@@ -87,23 +86,30 @@ public class SimplePriorityQueue<E> implements PriorityQueue<E> {
 	 */
 	@SuppressWarnings("unchecked")
 	public int insertSearch(E goal) {
-		int low = 0, high = size - 1, mid = high/2;
-		while(low <= high) {
+		int low = 0, high = size - 1, mid = high / 2;
+		while (low <= high) {
 			mid = low + (high - low) / 2;
-			if(compare(goal, (E)arr[mid]) == 0)
+			if (compare(goal, (E) arr[mid]) == 0)
 				return mid;
-			else if(compare(goal, (E)arr[mid]) < 0)
+			else if (compare(goal, (E) arr[mid]) < 0)
 				low = mid + 1;
 			else
 				high = mid - 1;
 		}
 		return low;
 	}
-	
+
+	/**
+	 * Inserts the specified elements into this priority queue.
+	 * 
+	 * @param obj1 - first element to compared
+	 * @param obj2 - second element to compared
+	 * @return comparison between obj1 and obj2
+	 */
 	@SuppressWarnings("unchecked")
 	private int compare(E obj1, E obj2) {
-		if(cmp == null)
-			return ((Comparable<? super E>)obj1).compareTo(obj2);
+		if (cmp == null)
+			return ((Comparable<? super E>) obj1).compareTo(obj2);
 		return cmp.compare(obj1, obj2);
 	}
 
@@ -117,7 +123,7 @@ public class SimplePriorityQueue<E> implements PriorityQueue<E> {
 		for (Object object : coll)
 			insert(object);
 	}
-	
+
 	/**
 	 * @return the number of elements in this priority queue
 	 */
@@ -137,13 +143,13 @@ public class SimplePriorityQueue<E> implements PriorityQueue<E> {
 	}
 
 	/**
-	 * Removes all of the elements from this priority queue. The queue will be
-	 * empty when this call returns.
+	 * Removes all of the elements from this priority queue. The queue will be empty
+	 * when this call returns.
 	 */
 	@SuppressWarnings("unchecked")
 	@Override
 	public void clear() {
-		arr =  (E[]) new Object[INITIAL_ARRAY_SIZE];
+		arr = (E[]) new Object[INITIAL_ARRAY_SIZE];
 		size = 0;
 	}
 }
