e3c7d24b7046718f1fffc7793203cc8087da5642
Nils Streedain <business@nilsstreedain.com>
2021-02-09 20:45:51 -0800

    Update SimplePriorityQueue.java

--- SimplePriorityQueue.java
+++ SimplePriorityQueue.java
@@ -17,28 +17,47 @@ public class SimplePriorityQueue<E> implements PriorityQueue<E> {
 		arr =  (E[]) new Object[INITIAL_ARRAY_SIZE];
 	}
 
-
 	@SuppressWarnings("unchecked")
 	public SimplePriorityQueue(Comparator<? super E> cmp) {
 		arr =  (E[]) new Object[INITIAL_ARRAY_SIZE];
 		this.cmp = cmp;
 	}
 
-
+	/**
+	 * Retrieves, but does not remove, the minimum element in this priority
+	 * queue.
+	 * 
+	 * @return the minimum element
+	 * @throws NoSuchElementException if the priority queue is empty
+	 */
+	@SuppressWarnings("unchecked")
 	@Override
 	public E findMin() throws NoSuchElementException {
-		// TODO Auto-generated method stub
-		return null;
+		if (size == 0)
+			throw new NoSuchElementException();
+		return (E) arr[size - 1];
 	}
-
-
+	
+	/**
+	 * Retrieves and removes the minimum element in this priority queue.
+	 * 
+	 * @return the minimum element
+	 * @throws NoSuchElementException if the priority queue is empty
+	 */
+	@SuppressWarnings("unchecked")
 	@Override
 	public E deleteMin() throws NoSuchElementException {
-		// TODO Auto-generated method stub
-		return null;
+		if (size == 0)
+			throw new NoSuchElementException();
+		size--;
+		return (E) arr[size - 1];
 	}
 
-
+	/**
+	 * Inserts the specified element into this priority queue.
+	 * 
+	 * @param item - the element to insert
+	 */
 	@SuppressWarnings("unchecked")
 	@Override
 	public void insert(Object item) {
@@ -46,9 +65,26 @@ public class SimplePriorityQueue<E> implements PriorityQueue<E> {
 		int index = insertSearch(goal);
 		
 		//actually add the thing at index
+		if (arr.length == size) {
+			Object[] newArr = new Object[arr.length * 2];
+			for (int i = 0; i < arr.length; i++)
+				newArr[i] = arr[i];
+			arr = newArr;
+		}
 		
+		for (int i = size; i > index; i--)
+			arr[i] = arr[i - 1];
+		
+		arr[index] = item;
+		size++;
 	}
 	
+	/**
+	 * Searches for the index to place the item in insert using a binary search.
+	 * 
+	 * @param goal - the goal for the binary search
+	 * @return mid - the index for the item to be placed
+	 */
 	@SuppressWarnings("unchecked")
 	public int insertSearch(E goal) {
 		int low = 0, high = arr.length - 1, mid = high/2;
@@ -56,7 +92,7 @@ public class SimplePriorityQueue<E> implements PriorityQueue<E> {
 			mid = (low + high) / 2;
 			if(((Comparable<? super E>)goal).compareTo((E)arr[mid]) == 0) 
 				break;
-			else if(((Comparable<? super E>)goal).compareTo((E)arr[mid]) == -1) 
+			else if(((Comparable<? super E>)goal).compareTo((E)arr[mid]) == 1) 
 				high = mid - 1;
 			else 
 				low = mid + 1;
@@ -64,32 +100,43 @@ public class SimplePriorityQueue<E> implements PriorityQueue<E> {
 		return mid;
 	}
 
-
+	/**
+	 * Inserts the specified elements into this priority queue.
+	 * 
+	 * @param coll - the collection of elements to insert
+	 */
 	@Override
-	public void insertAll(Collection coll) {
-		// TODO Auto-generated method stub
-		
+	public void insertAll(Collection<? extends E> coll) {
+		for (Object object : coll)
+			insert(object);
 	}
-
-
+	
+	/**
+	 * @return the number of elements in this priority queue
+	 */
 	@Override
 	public int size() {
 		return size;
 	}
 
-
+	/**
+	 * @return true if this priority queue contains no elements, false otherwise
+	 */
 	@Override
 	public boolean isEmpty() {
-		if (size == 0) {
+		if (size == 0)
 			return true;
-		}
 		return false;
 	}
 
-
+	/**
+	 * Removes all of the elements from this priority queue. The queue will be
+	 * empty when this call returns.
+	 */
+	@SuppressWarnings("unchecked")
 	@Override
 	public void clear() {
-		// TODO Auto-generated method stub
-		
+		arr =  (E[]) new Object[INITIAL_ARRAY_SIZE];
+		size = 0;
 	}
 }
